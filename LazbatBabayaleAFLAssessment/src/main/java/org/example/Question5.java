e1375548afeb0f3e60289c397aed245f7ee34133
Ibironkeyekinni <35920144+ibironke-yekinni@users.noreply.github.com>
2023-06-08 10:53:42 +0100

    Assessment_Highlight
    Welldone on the wonderful workdone @Lazbat.
    
    You did a good job, and I also observe your question 5 assessment is empty. Overall you tried.
    
    The hiring manager will reach out to you with feedback and next step.
    
    Happy Coding.

--- LazbatBabayaleAFLAssessment/src/main/java/org/example/Question5.java
+++ LazbatBabayaleAFLAssessment/src/main/java/org/example/Question5.java
@@ -1,4 +1,10 @@
 package org.example;
 
 public class Question5 {
+
+    @Override
+    public String toString() {
+        return "Question5 []";
+    }
+    //Please note that there was no solution here.
 }
--- LazbatBabayaleAFLAssessment/src/main/java/org/example/Question6.java
+++ LazbatBabayaleAFLAssessment/src/main/java/org/example/Question6.java
@@ -1,13 +1,16 @@
 package org.example;
 
 public class Question6 {
+    /**
+     * @param args
+     */
     public static void main(String[] args) {
         String input = "Hello";
         String input2 = "concatenate";
         String result = removeDuplicates(input);
         String result2 = removeDuplicates(input2);
-        System.out.println("The value of A" + result);
-        System.out.println("The value of B" + result2);
+        System.out.println("The value of A is " + result);
+        System.out.println("The value of B is " + result2);
 
     }
     public static String removeDuplicates(String str) {
